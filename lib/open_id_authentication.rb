225daafbf87dcf1efb8b8a267ea93d5506064901
Stefan Exner <stexmedia@googlemail.com>
2014-03-17 21:02:47 +0100

    added error

--- lib/open_id_authentication.rb
+++ lib/open_id_authentication.rb
@@ -4,6 +4,8 @@ require "open_id_authentication/controller_methods"
 require "open_id_authentication/railtie" if defined?(::Rails::Railtie)
 
 module OpenIdAuthentication
+  class InvalidOpenId << Exception; end
+
   # deprecated middleware creation
   def self.new(*args)
     raise "Use OpenIdAuthentication::Middleware"
